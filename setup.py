1347d9e2fd1657bf22c973049a31a0aeafc549b4
Victor Cabral <vccabral@gmail.com>
2013-05-13 03:05:29 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Victor Cabral',
+      author_email='vccabral@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+     )
+
